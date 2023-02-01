2d7fbc5b45e1e84b0f69cc3595ae3435e7fb2c6c
Priyanka Nayak <nayak.pri@northeastern.edu>
2023-02-01 08:24:27 -0500

    commit

--- api-routes/routes.js
+++ api-routes/routes.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
     const user = require('../controllers/user-controller');
     app.post('/v1/user',user.create);
-    app.get('/v1/user/userId',user.view);
-    app.put('/v1/user/userId',user.update);
+    app.get('/v1/user/:uuid',user.view);
+    app.put('/v1/user/:uuid',user.update);
     app.get('/healthz',user.health);
 }
