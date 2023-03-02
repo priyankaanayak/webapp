ed9d6a2c0002d6a7f6505abb7e787c989226b3e7
Priyanka Nayak <nayak.pri@northeastern.edu>
2023-03-01 22:32:23 -0500

    service

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -48,12 +48,7 @@ exports.create = (req, res) => {
               Message:
                 "Please provide all required fields - name, quantity, manufacturer, description, sku !",
             });
-          } else if (
-            isNaN(quantity) ||
-            quantity <= -1 ||
-            quantity >= 100 ||
-            parseInt(Number(quantity))
-          ) {
+          } else if (isNaN(quantity) || quantity <= -1 || quantity >= 100) {
             res.status(400).send({
               Message: "Please enter correct quantity!",
             });
